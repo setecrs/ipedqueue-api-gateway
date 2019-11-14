484cd7adf1ceee7d3e0d8b89a89d18c144990cef
Atila Romero <atilaromero@gmail.com>
2019-11-14 14:31:37 -0300

    -maxdepth 5

--- api/helpers/mkDVD.sh
+++ api/helpers/mkDVD.sh
@@ -15,10 +15,10 @@ trap "rm -rf $DVD" EXIT
 (
   flock -n 3 || exit 1
 
-  stdbuf -o 0 find "$@" -name indexador -prune -o -name 'hashlog.*' -print0 -o -name 'Lista de Arquivos.csv' -print0 |\
+  stdbuf -o 0 find "$@" -maxdepth 5 -name indexador -prune -o -name 'hashlog.*' -print0 -o -name 'Lista de Arquivos.csv' -print0 |\
   xargs -L1 -0 sha256sum |\
   tee "$1"/hashes.txt
 
-  find -L "$@" -name indexador -prune -o -name 'hashlog.*' -print0 -o -name 'Lista de Arquivos.csv' -print0 | xargs -0 tar c | tar x -C $DVD
+  find -L "$@" -maxdepth 5 -name indexador -prune -o -name 'hashlog.*' -print0 -o -name 'Lista de Arquivos.csv' -print0 | xargs -0 tar c | tar x -C $DVD
   mkisofs  -U -udf -iso-level 4 -allow-limited-size -f -o "$1"/DVD-anexo-laudo.iso "$1"/hashes.txt $DVD
 ) 3>>"$1"/hashes.txt || exit 1
